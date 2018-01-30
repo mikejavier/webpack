2794493c384af9560f58b07a75d81911bdf108f2
Michael Santillán <contato@michaelsantillan.com>
2018-01-30 16:24:23 -0300

    remove assets

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import image from './stevejobs.jpg';
-import data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  var myImage = new Image();
-  myImage.src = image;
-  element.appendChild(myImage);
-
-  console.log(data);
 
   return element;
 }
